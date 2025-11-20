419890ca637b1e887babd8e418e0ba619070bdd0
Ariel <ariellima610@hotmail.com>
2025-11-20 13:44:37 -0300

    adicionado um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log('Rodando o sistema de gerenciamento de jogos')
 console.log("Aplicação esta se conectando ao branco de dados")
 // mais console log de teste:
 console.log('O Paulo fez esse commit sem voce ver na aula!')
+console.log('Enviado para o Github')
